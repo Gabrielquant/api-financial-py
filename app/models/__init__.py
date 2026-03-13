c6e57a4e1f8d17d58fb09065b9c652e59b13f6a2
Gabriel Ferreira <gabriel.ferreiraa023@gmail.com>
2026-03-13 07:40:00 -0300

    fix: imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
-from app.models.category import Category, CategoryType  
-from app.models.transaction import Transaction  
-from app.models.user import User, UserRole  
+from app.models.category import Category as Category
+from app.models.category import CategoryType as CategoryType
+from app.models.transaction import Transaction as Transaction
+from app.models.user import User as User
+from app.models.user import UserRole as UserRole
